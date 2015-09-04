2018200fe31b3d8f69d3e83f31a9133ae013e1ae
soba1104 <shiba@rvm.jp>
2015-09-04 11:07:55 +0900

    segment 読み込みの雛形を追加

--- src/mach-o/load.c
+++ src/mach-o/load.c
@@ -62,6 +62,12 @@ static bool macho_loader_prepare_lc_segment_64(macho_loader *loader, struct load
     ERR("invalid segment_64 command\n");
     return false;
   }
+  for (i = 0; i < sizeof(command->segname) && command->segname[i]; i++);
+  if (i == sizeof(command->segname)) {
+    ERR("invalid segment name\n");
+    return false;
+  }
+  DEBUG("preparing segment: %s\n", command->segname);
 
   segs = realloc(loader->segments, sizeof(void*) * (loader->nsegs + 1));
   if (!segs) {
@@ -181,11 +187,68 @@ static bool macho_loader_prepare(macho_loader *loader) {
   if (!macho_loader_prepare_lc(loader, ptr, ncmds)) {
     return false;
   }
+  if (!loader->segments) {
+    ERR("no segment found\n");
+    return false;
+  }
+
+  return true;
+}
+
+static bool macho_loader_load_segment(macho_loader *loader, void *segment) {
+  uint64_t vmaddr, vmsize;
+  uint64_t fileoff, filesize;
+  vm_prot_t maxprot, initprot;
+  uint32_t nsects, flags;
+  const char *segname;
+
+#define INIT_VARS(type) do {\
+  type *s = segment; \
+  vmaddr = s->vmaddr; \
+  vmsize = s->vmsize; \
+  fileoff = s->fileoff; \
+  filesize = s->filesize; \
+  maxprot = s->maxprot; \
+  initprot = s->initprot; \
+  nsects = s->nsects; \
+  flags = s->flags; \
+  segname = s->segname; \
+} while(0)
+  if (loader->is32) {
+    INIT_VARS(struct segment_command);
+  } else {
+    INIT_VARS(struct segment_command_64);
+  }
+#undef INIT_VARS
+  DEBUG("loading segment: %s\n", segname);
+
+  return true;
+}
+
+static bool macho_loader_load_segments(macho_loader *loader) {
+  void **segments = loader->segments;
+  uint32_t nsegs = loader->nsegs, i;
+
+  assert(segments);
+  assert(nsegs > 0);
+
+  for (i = 0; i < nsegs; i++) {
+    void *segment = segments[i];
+    if (!macho_loader_load_segment(loader, segment)) {
+      ERR("failed to load segment\n");
+      return false;
+    }
+  }
 
   return true;
 }
 
 static bool macho_loader_load(macho_loader *loader) {
+  if (!macho_loader_load_segments(loader)) {
+    ERR("failed to load segments\n");
+    return false;
+  }
+
   return true;
 }
 
