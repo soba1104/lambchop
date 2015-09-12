01a44fb7bdc1d8ba339c85158d5194c318d1e045
soba1104 <shiba@rvm.jp>
2015-09-12 23:58:45 +0900

    対応命令を増やす

--- src/vm.c
+++ src/vm.c
@@ -58,11 +58,14 @@ static const char *regnames[] = {
 #define OPCODE_PUSH_RSI (0x50 | REGISTER_RSI)
 #define OPCODE_PUSH_RDI (0x50 | REGISTER_RDI)
 
+#define OPCODE_SUB 0x83
+
 #define OPCODE_MOV_0X89 0x89
 
 #define MODRM_REG(modrm) (((modrm) & 0x38) >> 3)
 #define MODRM_MOD(modrm) (((modrm) & 0xc0) >> 6)
 #define MODRM_RM(modrm) ((modrm) & 0x07)
+#define MODRM_OP_EXT(modrm) (((modrm) & 0xc0) >> 6)
 #define MODRM_MOD_MEM_DISP0 0
 #define MODRM_MOD_MEM_DISP8 1
 #define MODRM_MOD_MEM_DISP32 2
@@ -106,6 +109,13 @@ static inline uint8_t fetch_opcode(uint64_t *ipp) {
   return opcode;
 }
 
+static inline char fetch_imm_sb(uint64_t *ipp) {
+  uint64_t ip = *ipp;
+  char imm = (*((char*)ip));
+  *ipp = (ip + 1);
+  return imm;
+}
+
 static int vm_main(uint8_t *p, lambchop_logger *logger) {
   uint64_t rip = (uint64_t)p;
 
@@ -124,6 +134,14 @@ static int vm_main(uint8_t *p, lambchop_logger *logger) {
         dst = regnames[modrm_rex_mod_rm(modrm, rex)];
         assert(IS_REX_W(rex));
         DEBUG("MOV %s %s\n", src, dst);
+        break;
+      case OPCODE_SUB:
+        modrm = fetch_modrm(&rip);
+        dst = regnames[modrm_rex_mod_rm(modrm, rex)];
+        {
+          char imm = fetch_imm_sb(&rip);
+          DEBUG("SUB 0x%x %s\n", imm, dst);
+        }
         break;
       default:
         ERR("unsupported opcode 0x%x\n", opcode);
