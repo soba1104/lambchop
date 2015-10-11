05838bd8a90ef7c9ee0fbe405d39f7506ade0b2e
soba1104 <shiba@rvm.jp>
2015-10-11 21:43:11 +0900

    workq_kernreturn で WQOPS_THREAD_RETURN のときの分岐を追加

--- src/vm.c
+++ src/vm.c
@@ -504,6 +504,7 @@ static void syscall_callback_workq_kernreturn(const syscall_entry *syscall, void
   uint32_t arg3 = get_r10(cpu);
 
   // 以下は libpthread の kern/workqueue_internal.h から持ってきたマクロ
+#define WQOPS_THREAD_RETURN 4
 #define WQOPS_QUEUE_NEWSPISUPP  0x10
 #define WQOPS_QUEUE_REQTHREADS  0x20
 #define WQOPS_QUEUE_REQTHREADS2 0x30
@@ -529,6 +530,9 @@ static void syscall_callback_workq_kernreturn(const syscall_entry *syscall, void
       break;
     case WQOPS_QUEUE_REQTHREADS:
       DEBUG("SYSCALL: workq_kernreturn(REQTHREADS, reqcount=0x%x, priority=0x%x)\n", arg2, arg3);
+      break;
+    case WQOPS_THREAD_RETURN:
+      DEBUG("SYSCALL: workq_kernreturn(RETURN)\n");
       break;
     default:
       assert(false);
